2704e50803c08de6149eb71d9e51628f11fda66e
David De La Cruz <david.delacruzs@outlook.com>
2024-04-15 22:44:39 -0500

    Se agrega código para consultar paises por capital

---
+++ src/app/shared/components/search-box/search-box.component.ts
@@ -0,0 +1,19 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'shared-search-box',
+  templateUrl: './search-box.component.html',
+  styles: ``
+})
+export class SearchBoxComponent {
+
+  @Input()
+  placeholder: string = '';
+
+  @Output()
+  onValue = new EventEmitter<string>();
+
+  emitValue(value: string): void {
+    this.onValue.emit(value);
+  }
+}
