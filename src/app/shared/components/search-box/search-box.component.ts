9108c3c2c12d50acf182709f25a5fac901433df2
David De La Cruz <david.delacruzs@outlook.com>
2024-04-30 10:46:18 -0500

    Se agregan mejoras para persistir la información en caché

--- src/app/shared/components/search-box/search-box.component.ts
+++ src/app/shared/components/search-box/search-box.component.ts
@@ -1,14 +1,23 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
+import { Subject, Subscription, debounceTime } from 'rxjs';
 
 @Component({
   selector: 'shared-search-box',
   templateUrl: './search-box.component.html',
   styles: ``
 })
-export class SearchBoxComponent {
+export class SearchBoxComponent implements OnInit, OnDestroy {
+  private debouncer: Subject<string> = new Subject<string>();
+  private debouncerSubscription?: Subscription;
 
   @Input()
   placeholder: string = '';
+
+  @Input()
+  initialValue: string = '';
+
+  @Output()
+  onDebounce = new EventEmitter<string>();
 
   @Output()
   onValue = new EventEmitter<string>();
@@ -16,4 +25,23 @@ export class SearchBoxComponent {
   emitValue(value: string): void {
     this.onValue.emit(value);
   }
+
+  onKeyPress(searchTerm: string) {
+    this.debouncer.next(searchTerm);
+  }
+
+  ngOnInit(): void {
+    this.debouncerSubscription = this.debouncer
+      .pipe(
+        debounceTime(300)
+      )
+      .subscribe(value => {
+        this.onDebounce.emit(value);
+      })
+  }
+
+  ngOnDestroy(): void {
+    this.debouncerSubscription?.unsubscribe();
+  }
+
 }
